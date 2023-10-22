7cb9b0928cbd1abc74d96da06481e0048fc486c4
jinnygo <ahn43541@gmail.com>
2023-10-22 16:40:13 +0900

    first commit

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,41 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ */
+
+var name = '코드팩토리';
+console.log(name);
+
+var age = '32';
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+
+// newJeans = '123';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ */
+
+let girlFriend;
+console.log(girlFriend);
+
+// const는 값을 초기화를 하면서 선언해야한다. (선언과 할당이 동시에)
